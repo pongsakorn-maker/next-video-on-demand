70b531c051f2dba9a10c256e53f1156726b62fdf
pongsakorn-maker <pongsakorn.reo@gmail.com>
2021-01-05 19:12:20 +0700

    added bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import App from 'next/app'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import App from "next/app";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default class _app extends App {
   render() {
-    const {Component} = this.props
+    const { Component } = this.props;
     return (
       <div>
-      <Navbar/>
-        <Component/>
-      <Footer/>
+        <Head>
+          <title>Video On Demand</title>
+          <link rel="icon" href="/book.png" />
+          <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+            crossOrigin="anonymous"
+          />
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+            crossorigin="anonymous"
+          ></script>
+        </Head>
+        <Navbar />
+        <Component />
+        <Footer />
       </div>
-    )
+    );
   }
 }
